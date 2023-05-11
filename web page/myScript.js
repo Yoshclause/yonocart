274e10b2b5da6d6b4b9ffcf612d44a4892e5d8c7
Yoshclause <122715560+Yoshclause@users.noreply.github.com>
2023-05-11 07:46:44 +0530

    Delete web page directory

--- web page/myScript.js
+++
@@ -1,8 +0,0 @@
-const product_type = [
-  "Electrical equipments",
-  "Mechanical privilages",
-  "Mechanical tools",
-];
-const products = ["Hardware products", "Engineering tools", "cart board"];
-document.getElementById("variableNote").innerHTML =
-  product_type.concat(products);
