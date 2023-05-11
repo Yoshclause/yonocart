fbc07342154da87bf213bb1cd882cb9de6daf976
Yoshclause <122715560+Yoshclause@users.noreply.github.com>
2023-05-11 07:48:45 +0530

    Add files via upload

---
+++ myScript.js
@@ -0,0 +1,8 @@
+const product_type = [
+  "Electrical equipments",
+  "Mechanical privilages",
+  "Mechanical tools",
+];
+const products = ["Hardware products", "Engineering tools", "cart board"];
+document.getElementById("variableNote").innerHTML =
+  product_type.concat(products);
